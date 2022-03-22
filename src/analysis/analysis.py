38cbbb42dfafa6506802f5921793e1f195d7e7cc
AnonymousHankle <37787056+AnonymousHankle@users.noreply.github.com>
2022-03-21 22:51:28 -0400

    Working on refactoring single factor analysis. Start to work on tutorial examples

---
+++ src/analysis/analysis.py
@@ -0,0 +1,20 @@
+
+from abc import abstractmethod
+
+class Analysis():
+
+    @abstractmethod
+    def __init__():
+        pass
+
+    @abstractmethod
+    def run():
+        pass
+
+    @abstractmethod
+    def generate_report():
+        pass
+
+    @abstractmethod
+    def generate_graph():
+        pass
